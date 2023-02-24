fe5ea3bb05e5b1998d38fd6cfeb535e05341c0d8
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-02-24 11:30:09 -0300

    modified create_tables to plural names

--- create_tables.sql
+++ create_tables.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA `library` ;
 
 use library;
 
-CREATE TABLE libro(
+CREATE TABLE libros(
 	id_libro INT,
     titulo VARCHAR(20),
     id_autor INT,
@@ -10,50 +10,50 @@ CREATE TABLE libro(
     cant_disponible INT,
     PRIMARY KEY(id_libro));
     
-CREATE TABLE genero(
+CREATE TABLE generos(
 id_genero INT,
 descripcion VARCHAR(20),
 	
     PRIMARY KEY(id_genero));
     
-CREATE TABLE editorial(
+CREATE TABLE editoriales(
 id_editorial INT,
 descripcion VARCHAR(20),
 	
     PRIMARY KEY(id_editorial));
 
-CREATE TABLE nacionalidad(
+CREATE TABLE nacionalidades(
 	id_nacionalidad INT,
     descripcion VARCHAR(20),
     
     PRIMARY KEY(id_nacionalidad));
 
 
-CREATE TABLE autor(
+CREATE TABLE autores(
 	id_autor INT,
     nombre VARCHAR(20),
     apellidos VARCHAR(20),
     id_nacionalidad INT,
     PRIMARY KEY(id_autor),
-FOREIGN KEY(id_nacionalidad) REFERENCES nacionalidad(id_nacionalidad)
+FOREIGN KEY(id_nacionalidad) REFERENCES nacionalidades(id_nacionalidad)
 	
 );
 
 
 
 
-ALTER TABLE libro
+ALTER TABLE libros
 ADD id_genero INT,
 ADD id_editorial INT,
-ADD CONSTRAINT `FK_EDITORIAL` FOREIGN KEY (id_editorial) references editorial(id_editorial),
-ADD CONSTRAINT `FK_AUTOR` FOREIGN KEY (id_autor) references autor(id_autor),
-ADD CONSTRAINT `FK_GENERO` FOREIGN KEY (id_genero) references genero(id_genero);
+ADD CONSTRAINT `FK_EDITORIALES` FOREIGN KEY (id_editorial) references editoriales(id_editorial),
+ADD CONSTRAINT `FK_AUTORES` FOREIGN KEY (id_autor) references autores(id_autor),
+ADD CONSTRAINT `FK_GENEROS` FOREIGN KEY (id_genero) references generos(id_genero);
 
 
 
 
 
-CREATE TABLE cliente(
+CREATE TABLE clientes(
 	id_cliente INT,
     nombre VARCHAR(20),
     apellidos VARCHAR(20),
@@ -65,12 +65,12 @@ CREATE TABLE cliente(
     PRIMARY KEY(id_cliente));
 
 
-CREATE TABLE ejemplar(
+CREATE TABLE ejemplares(
 	id_ejemplar INT,
     id_libro INT,
     fecha_tomado DATE,
 	id_cliente INT,
     PRIMARY KEY(id_ejemplar),
-    FOREIGN KEY(id_libro) REFERENCES libro(id_libro),
-    FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente)
+    FOREIGN KEY(id_libro) REFERENCES libros(id_libro),
+    FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
     );
