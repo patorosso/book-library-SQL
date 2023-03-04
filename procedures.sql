8b93a9f9cf96c3fd0a3011916e16e9c56e50c8ff
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-03-04 00:52:50 -0300

    added second procedure

--- procedures.sql
+++ procedures.sql
@@ -1,5 +1,7 @@
 use library;
 
+/* primer procedure */
+
 drop procedure if exists `select de campo ordenado`;
 
 delimiter $$
@@ -22,4 +24,34 @@ delimiter ;
 
 
 call `select de campo ordenado` ('titulo',''); /* 'desc' or '' */
+
+
+/* segundo procedure */
+
+
+drop procedure if exists `select de campo ordenado`;
+
+delimiter $$
+create procedure `select de campo ordenado`
+(
+/*params*/
+in campo varchar(20),
+in orden varchar(4)
+)
+begin
+/* query para ordenar por el campo, todo esto en la tabla libros */
+set @sentencia = concat ('select * from libros order by ',campo,' ',orden);
+
+prepare dummy from @sentencia;
+execute dummy;
+deallocate prepare dummy;
+end
+$$
+delimiter ;
+
+
+call `select de campo ordenado` ('titulo',''); /* 'desc' or '' */
+
+
+
 
