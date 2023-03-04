50db31fd113d22d2c29d19bc69b2a3f2bf5ec1f4
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-03-03 23:33:58 -0300

    fixed issue by applying delimiters

--- functions.sql
+++ functions.sql
@@ -1,7 +1,8 @@
-use library;
+DELIMITER //
 CREATE FUNCTION count_books_by_author(author_id INT) RETURNS INT
 BEGIN
   DECLARE book_count INT;
   SELECT COUNT(*) INTO book_count FROM libros WHERE author_id = author_id;
   RETURN book_count;
-END;
+END //
+DELIMITER ;
